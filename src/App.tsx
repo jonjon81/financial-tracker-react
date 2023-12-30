dc818ce6cc7e4603f473007cbcb08d7716293353
Jon Atlas <jjjatlas@hotmail.com>
2023-12-30 11:17:03 +0100

    Add loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import InvoicesWidget from './components/InvoicesWidget';
 import { useInvoice } from './context/InvoiceContexts';
 import { useTransaction } from './context/TransactionContexts';
 import { Invoice } from './types/Invoice';
+import './App.css';
 
 const App: React.FC = () => {
   const { state: invoiceState } = useInvoice();
   const { state: transactionState } = useTransaction();
   const [invoicesLast30, setInvoicesLast30] = useState<number>(0);
+  const [isLoading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const calculateInvoicesCreatedLast30Days = (invoicesData: Invoice[]): number => {
@@ -16,25 +18,32 @@ const App: React.FC = () => {
       thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
 
       const invoicesLast30Days = invoicesData.filter((invoice) => new Date(invoice.creationDate) >= thirtyDaysAgo);
-
       return invoicesLast30Days.length;
     };
 
     const invoicesLast30DaysCount = calculateInvoicesCreatedLast30Days(invoiceState.invoices);
     setInvoicesLast30(invoicesLast30DaysCount);
+    if (invoiceState.invoices.length > 0) {
+      setLoading(false);
+    }
   }, [invoiceState.invoices]);
 
   return (
     <div className="container">
       <h1>Financial Dashboard</h1>
-      <div className="dashboard">
-        <SummaryWidget invoicesLast30Days={invoicesLast30} transactions={transactionState.transactions} />
-        <InvoicesWidget
-          invoices={invoiceState.invoices}
-          setInvoices={() => {}}
-          transactions={transactionState.transactions}
-        />
-      </div>
+
+      {isLoading ? (
+        <div className="d-flex justify-content-center spinner-overlay">
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      ) : (
+        <div className="dashboard">
+          <SummaryWidget invoicesLast30Days={invoicesLast30} transactions={transactionState.transactions} />
+          <InvoicesWidget transactions={transactionState.transactions} />
+        </div>
+      )}
     </div>
   );
 };
