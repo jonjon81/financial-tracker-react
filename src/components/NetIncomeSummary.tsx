3eb67d8bf47be9c144e8556361aaf2154fb9f58c
Jon Atlas <jjjatlas@hotmail.com>
2024-01-02 19:37:11 +0100

    Moved expense state up

--- src/components/NetIncomeSummary.tsx
+++ src/components/NetIncomeSummary.tsx
@@ -4,12 +4,16 @@ import { Invoice } from '../types/Invoice';
 interface NetIncomeSummaryProps {
   incomeTotalPrevious12Months: number;
   incomeTotalPrevious24Months: number;
+  expenseTotalPrevious12Months: number;
+  expenseTotalPrevious24Months: number;
   transactions: Invoice[];
 }
 
 const NetIncomeSummary: React.FC<NetIncomeSummaryProps> = ({
   incomeTotalPrevious12Months,
   incomeTotalPrevious24Months,
+  expenseTotalPrevious12Months,
+  expenseTotalPrevious24Months,
 }) => {
   const difference = incomeTotalPrevious12Months - incomeTotalPrevious24Months;
   const differencePercentage =
@@ -19,6 +23,9 @@ const NetIncomeSummary: React.FC<NetIncomeSummaryProps> = ({
       <h2>Net Income Summary</h2>
       <p>Income Total Previous 12 Months: {incomeTotalPrevious12Months}</p>
       <p>Income Total Previous 24 Months: {incomeTotalPrevious24Months}</p>
+      <p>Difference % = {differencePercentage}</p>
+      <p>Expense Total Previous 12 Months: {expenseTotalPrevious12Months}</p>
+      <p>Expense Total Previous 24 Months: {expenseTotalPrevious24Months}</p>
       <p>Difference % = {differencePercentage}</p>
     </div>
   );
--- src/components/SummaryWidget.tsx
+++ src/components/SummaryWidget.tsx
@@ -8,10 +8,12 @@ import NetIncomeSummary from './NetIncomeSummary';
 import { useContext } from 'react';
 import { InvoiceContext } from '../context/InvoiceContexts';
 import { Invoice } from '../types/Invoice';
+import { Transaction } from '../types/Transaction';
 
 const SummaryWidget = () => {
   const { state: transactionState } = useTransaction();
   const { state: invoiceState } = useContext(InvoiceContext);
+  const { transactions } = transactionState;
   const { invoices } = invoiceState;
 
   const filterInvoicesByDynamicMonths = (monthsAgo: number): Invoice[] => {
@@ -37,6 +39,38 @@ const SummaryWidget = () => {
     0
   );
 
+  const filterTransactionsByDynamicMonths = (monthsAgo: number): Transaction[] => {
+    const currentDate = new Date();
+    const endDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+    const startDate = new Date(endDate.getFullYear(), endDate.getMonth() - monthsAgo, 1);
+
+    return transactions.filter((transaction: Transaction) => {
+      const transactionDate = new Date(transaction.transactionDate);
+      return transactionDate >= startDate && transactionDate <= endDate && transaction.category === 'expense';
+    });
+  };
+
+  const filterTransactions: (monthsAgo: number) => Transaction[] = (monthsAgo) =>
+    filterTransactionsByDynamicMonths(monthsAgo);
+
+  const previous12MonthsTransactions = filterTransactions(12);
+  const previous24MonthsTransactions = filterTransactions(24);
+
+  const expensesTotalPrevious12Months = previous12MonthsTransactions.reduce(
+    (total: number, transaction: Transaction) => total + transaction.amount,
+    0
+  );
+  const expensesTotalPrevious24Months = previous24MonthsTransactions.reduce(
+    (total: number, transaction: Transaction) => total + transaction.amount,
+    0
+  );
+
+  const difference = expensesTotalPrevious12Months - expensesTotalPrevious24Months;
+  const differencePercentageExpense =
+    expensesTotalPrevious24Months !== 0 ? ((difference / expensesTotalPrevious24Months) * 100).toFixed(2) : 'N/A';
+
+  const absoluteTotalExpense = Math.abs(expensesTotalPrevious12Months);
+
   return (
     <div className="summary-main-container d-flex justify-content-between">
       <CashBalance transactions={[]} />
@@ -45,6 +79,8 @@ const SummaryWidget = () => {
       <NetIncomeSummary
         incomeTotalPrevious12Months={incomeTotalPrevious12Months}
         incomeTotalPrevious24Months={incomeTotalPrevious24Months}
+        expenseTotalPrevious12Months={expensesTotalPrevious12Months}
+        expenseTotalPrevious24Months={expensesTotalPrevious24Months}
         transactions={[]}
       />
       <Last30Days transactions={transactionState.transactions} />
