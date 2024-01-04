5a46be04cedbbacb1e48f0ee71b1d23e8b2573d7
Jon Atlas <jjjatlas@hotmail.com>
2024-01-04 12:27:13 +0100

    move component order

--- src/App.tsx
+++ src/App.tsx
@@ -28,8 +28,8 @@ const App: React.FC = () => {
       ) : (
         <div className="dashboard">
           <SummaryWidget />
-          <BarChart />
           <InvoicesWidget transactions={transactionState.transactions} />
+          <BarChart />
         </div>
       )}
     </div>
